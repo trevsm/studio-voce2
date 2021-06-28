c8fe6e2a7ec49eeef22d0f747ca67460058a6b46
trevsm <trevorsm@pm.me>
2021-06-27 20:23:30 -0600

    Added calendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,14 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import { Row, Col } from "react-bootstrap"
 import { Page } from "../components/default"
 import { Section } from "../components/default"
+import "react-calendar/dist/Calendar.css"
 
-export default function Calendar() {
+import Calendar from "react-calendar"
+
+export default function Component() {
+    const [value, onChange] = useState(new Date())
     return (
         <Page>
             <Section>
                 <Row>
-                    <Col>asdf</Col>
+                    <Col>
+                        <h4 style={{ textAlign: "center", opacity: ".8" }}>
+                            There are currently no upcoming events.
+                        </h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div
+                            className="calendar-parent"
+                            style={{
+                                margin: "0 auto",
+                                width: "fit-content",
+                            }}
+                        >
+                            <Calendar value={value} onChange={onChange} />
+                        </div>
+                    </Col>
                 </Row>
             </Section>
         </Page>
